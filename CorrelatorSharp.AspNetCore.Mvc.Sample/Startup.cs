9d5b5478b9df1c10de02a11d767930e311bb459f
Jason Dryhurst-Smith <jason@codat.io>
2018-02-14 15:53:00 +0000

    add the async filter code and the sample app in core 2

---
+++ CorrelatorSharp.AspNetCore.Mvc.Sample/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CorrelatorSharp.AspNetCore.Mvc;
+
+namespace CorrelatorSharp.AspNetCore.Mvc.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(configuration =>
+            {
+                configuration.Filters.Add(new CorrelationIdActionFilter());
+            });
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
