002f2a0847092677aa8b6e76c99cf760ffd02315
Jason Dryhurst-Smith <jason@codat.io>
2018-11-01 15:36:22 +0000

    add the parent ID as a specified header/field value

--- CorrelatorSharp.AspNetCore.Mvc.Sample/Program.cs
+++ CorrelatorSharp.AspNetCore.Mvc.Sample/Program.cs
@@ -5,13 +5,11 @@ namespace CorrelatorSharp.AspNetCore.Mvc.Sample
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static void Main(string[] args) 
+            => BuildWebHost(args).Run();
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args) 
+            => WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
     }
--- CorrelatorSharp.AspNetCore.Mvc.Sample/Startup.cs
+++ CorrelatorSharp.AspNetCore.Mvc.Sample/Startup.cs
@@ -2,27 +2,23 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CorrelatorSharp.AspNetCore.Mvc;
 
 namespace CorrelatorSharp.AspNetCore.Mvc.Sample
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) 
+            => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc(configuration =>
-            {
-                configuration.Filters.Add(new CorrelationIdActionFilter());
-            });
-        }
-        
+            => services.AddMvc(configuration
+                => 
+                {
+                    configuration.Filters.Add(new CorrelationIdActionFilter());
+                });
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
